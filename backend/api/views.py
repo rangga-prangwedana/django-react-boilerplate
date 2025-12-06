df730fa1d2d1c246c29b3d3e058618094758bf69
Rangga Prangwedana <vergil1st@gmail.com>
2025-12-06 12:28:40 +0700

    feat: Called and added UserSerializer class to views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = User
+    permission_classes = [AllowAny]
